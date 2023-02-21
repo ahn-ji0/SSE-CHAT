0bca52bbf1fad00a6390b38c1802f89bcc1bb6d3
Jiyoung <ahn.jiyoung15@gmail.com>
2023-02-21 11:33:45 +0900

    Feat: GET /chat/messages

--- src/main/java/com/sse/ssechat/controller/ChatController.java
+++ src/main/java/com/sse/ssechat/controller/ChatController.java
@@ -5,6 +5,7 @@ import com.sse.ssechat.Response;
 import com.sse.ssechat.domain.WriteMessageResponse;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class ChatController {
         chatMessageList.add(message);
 
         return new Response("SUCCESS","메세지가 작성되었습니다.", new WriteMessageResponse(message.getId()));
+    }
+
+    @GetMapping("/messages")
+    public Response getMessages(){
+        return new Response("SUCCESS","성공", chatMessageList);
     }
 
 }
--- src/main/java/com/sse/ssechat/domain/ChatMessage.java
+++ src/main/java/com/sse/ssechat/domain/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.sse.ssechat.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Getter;
 @Getter
 public class ChatMessage {
     private long id;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
     private String authorName;
     private String content;
