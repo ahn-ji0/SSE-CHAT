5511a2f975293c54ee9f4383982178b2ecaa7fe6
Jiyoung <ahn.jiyoung15@gmail.com>
2023-02-21 11:40:17 +0900

    Feat: writeMessage request body 추가

--- src/main/java/com/sse/ssechat/controller/ChatController.java
+++ src/main/java/com/sse/ssechat/controller/ChatController.java
@@ -2,11 +2,13 @@ package com.sse.ssechat.controller;
 
 import com.sse.ssechat.domain.ChatMessage;
 import com.sse.ssechat.Response;
+import com.sse.ssechat.domain.WriteMessageRequest;
 import com.sse.ssechat.domain.WriteMessageResponse;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,8 @@ public class ChatController {
 
 
     @PostMapping("/writeMessage")
-    public Response writeMessage(){
-        ChatMessage message = new ChatMessage("안지영", "안녕하세요");
+    public Response writeMessage(@RequestBody WriteMessageRequest writeMessageRequest){
+        ChatMessage message = new ChatMessage(writeMessageRequest.authorName(), writeMessageRequest.content());
 
         chatMessageList.add(message);
 
